ada62e60fe835e20abe677c85d3d76647e3cccf1
gofogo <promoted.go@gmail.com>
2023-11-06 18:39:52 +0900

    init

---
+++ service.js
@@ -0,0 +1,31 @@
+"use strict";
+
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const sshpk = require("sshpk");
+const axios = require("axios");
+const qs = require("query-string");
+const httpSignature = require("http-signature");
+const bodyParser = require("body-parser");
+const app = module.exports = express();
+const port = process.env.PORT || 2321;
+
+app.use(express.static(path.join(__dirname, "/View")));
+app.use(bodyParser.json());
+
+app.post("/", async (req, res) => {
+    res.end();
+  });
+
+  app.get("/", function (req, res) {
+    res.sendFile("/src/index.html");
+  });
+
+  app.get("/oauth/callback", (req, res) => {
+    res.json({ok:true});
+  });
+
+  app.listen(port, () => {
+    console.log(`Open at port http://localhost:${port}`);
+  });
