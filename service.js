e1eb989269434a5b94f4fee3bd9865e17b045c09
gofogo <promoted.go@gmail.com>
2023-11-08 23:09:43 +0900

    Update service.js

--- service.js
+++ service.js
@@ -34,7 +34,7 @@ app.post("/oauth/callback", (req, res) => {
 
   // console.log(url);
 
-  res.redirect(`/oauth/callback?coolish://callback?https://appleid.apple.com/auth/authorize?${queryString}`);
+  res.redirect(`coolish://callback?${queryString}`);
   // return res.json({ ok: true });
 });
 
