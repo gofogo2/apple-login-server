c8cd956a868f32a812894c226bae510ac585c7a7
gofogo <promoted.go@gmail.com>
2023-11-08 22:57:50 +0900

    Update service.js

--- service.js
+++ service.js
@@ -34,7 +34,7 @@ app.post("/oauth/callback", (req, res) => {
 
   console.log(url);
 
-  res.redirect(`/oauth/callback?${queryString}`);
+  res.redirect(`https://appleid.apple.com/auth/authorize?${queryString}`);
   // return res.json({ ok: true });
 });
 
