17d376aa65d75e5badcb67bb4e1741330092ee19
gofogo <promoted.go@gmail.com>
2023-11-08 22:08:07 +0900

    Update service.js

--- service.js
+++ service.js
@@ -12,7 +12,6 @@ const app = module.exports = express();
 const port = process.env.PORT || 2321;
 
 app.use(express.static(path.join(__dirname, "/View")));
-// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/", async (req, res) => {
@@ -23,16 +22,13 @@ app.post("/", async (req, res) => {
     res.sendFile("/src/index.html");
   });
 
-  app.post("/oauth/callback", (req, res) => {
-    // console.log(req.body);
-    const queryString = qs.stringify(req.body);
-    // const queryString = new URLSearchParams(req.body).toString();
-    // const getBody = req.bodyParser.urlencoded();
-    // console.log(getBody);
-    // console.log(req.body.state);
-    const param = req.body;
 
-    const url = `https://playgalaxy.net/oauth/callback?coolish://callback?${queryString}?client_id=net.playgalaxy.applogin`;
+  //post url
+  app.post("/oauth/callback", (req, res) => {
+    const queryString = qs.stringify(req.body);
+    const param = req.body;
+// https 인증서 발급된 도메인 등록
+    const url = `https://playgalaxy.net/oauth/callback?coolish://callback?${queryString}`;
 
     console.log(url);
 
